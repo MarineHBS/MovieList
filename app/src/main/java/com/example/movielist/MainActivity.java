0f37afec8ecf5913fce412b050bee65252a08811
Szlovák Máté <marinehbs@gmail.com>
2019-03-25 09:22:44 +0100

    travis changes

--- app/src/main/java/com/example/movielist/MainActivity.java
+++ app/src/main/java/com/example/movielist/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.movielist;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton add = findViewById(R.id.addNewMovie);
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Snackbar.make(view, "movie added", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            }
+
+        });
+
     }
 }

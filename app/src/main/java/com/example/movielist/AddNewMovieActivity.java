61c9943f7425c879d163efc4d77e97e146b416de
Szlovák Máté <marinehbs@gmail.com>
2019-03-25 09:27:52 +0100

    gradlew permission changed

---
+++ app/src/main/java/com/example/movielist/AddNewMovieActivity.java
@@ -0,0 +1,14 @@
+package com.example.movielist;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AddNewMovieActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_movie);
+    }
+
+}

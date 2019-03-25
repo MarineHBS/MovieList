55f89aff6a699909d95740554cc024556da4909b
Szlovák Máté <marinehbs@gmail.com>
2019-03-25 10:58:26 +0100

    Switching between activities work, model class added

--- app/src/main/java/com/example/movielist/AddNewMovieActivity.java
+++ app/src/main/java/com/example/movielist/AddNewMovieActivity.java
@@ -2,8 +2,11 @@ package com.example.movielist;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.content.Intent;
 
 public class AddNewMovieActivity extends Activity {
 
@@ -11,11 +14,28 @@ public class AddNewMovieActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_movie);
+
         Spinner genresSpinner = (Spinner) findViewById(R.id.genres_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.genres_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genresSpinner.setAdapter(adapter);
+
+        Spinner ratingsSpinner = (Spinner) findViewById(R.id.ratings_spinner);
+        ArrayAdapter<CharSequence> adapter_ = ArrayAdapter.createFromResource(this,
+                R.array.ratings_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ratingsSpinner.setAdapter(adapter_);
+
+        Button addNewMovieButton = (Button) findViewById(R.id.add_new_movie_button);
+        addNewMovieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddNewMovieActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+        );
     }
 
 }
--- app/src/main/java/com/example/movielist/Models/Movie.java
+++ app/src/main/java/com/example/movielist/Models/Movie.java
@@ -1,4 +1,9 @@
 package com.example.movielist.Models;
 
 public class Movie {
+    String title;
+    String description;
+    Integer year;
+    String genre;
+    Integer rating;
 }
